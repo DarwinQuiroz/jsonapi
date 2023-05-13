7bafac5ad4167f3512613fe55e6bacc3bc8cd9d1
DarwinQuiroz <darwin_quiroz04@hotmil.com>
2023-05-13 10:40:17 -0500

    Mostrar todos los artiículos

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Article;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
-use App\Models\Article;
-use Illuminate\Http\Request;
+use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
 {
+    /**
+     * Summary of index
+     * @return \App\Http\Resources\ArticleCollection
+     */
+    public function index(): ArticleCollection
+    {
+        return ArticleCollection::make(Article::all());
+    }
+
+
     /**
      * Summary of show
      * @param \App\Models\Article $article
